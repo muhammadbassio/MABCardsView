965cfce176eaedb01492e6788247dd1e510a2920
muhammadbassio <muhammadbassio@Muhammads-MacBook-Pro.local>
2014-11-29 19:45:01 +0200

    Initial Commit

---
+++ CardsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardsDemo
+//
+//  Created by Muhammad Bassio on 11/29/14.
+//  Copyright (c) 2014 Muhammad Bassio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ CardsDemoTests/CardsDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  CardsDemoTests.swift
+//  CardsDemoTests
+//
+//  Created by Muhammad Bassio on 11/29/14.
+//  Copyright (c) 2014 Muhammad Bassio. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CardsDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
